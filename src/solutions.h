df98a6ece190bf8b031e7f223b56faeb3d1c886e
JingXu <jx_tdem@qq.com>
2023-04-21 14:22:18 +0800

    P5 6 11 15

--- src/solutions.h
+++ src/solutions.h
@@ -53,6 +53,7 @@ public:
         return {-1, -1};
     }
 
+public:
     // 2.Add Two Numberes
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
         ListNode *dummyHead = new ListNode(0);
@@ -71,6 +72,58 @@ public:
         return dummyHead->next;
     }
 
+public:
+    // 5.Longest Palindromic Substring
+    std::string longestPalindrome(std::string s) {
+        if (s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = std::max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substr(start, end - start + 1);
+    }
+
+private:
+    int expandAroundCenter(std::string s, int left, int right) {
+        int l = left, r = right;
+        while (l >= 0 && r < s.length() && s[l] == s[r]) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
+    }
+
+public:
+    // 6.Zigzag Conversion
+    std::string convert(std::string s, int numRows) {
+        if (numRows == 1) return s;
+        std::string answer;
+        int n = s.size();
+        int charsInSection = 2 * (numRows - 1);
+        for (int currRow = 0; currRow < numRows; ++currRow) {
+            int index = currRow;
+            while (index < n) {
+                answer += s[index];
+                if (currRow != 0 && currRow != numRows - 1) {
+                    int charsInBetween = charsInSection - 2 * currRow;
+                    int secondIndex = index + charsInBetween;
+                    if (secondIndex < n) {
+                        answer += s[secondIndex];
+                    }
+                }
+                index += charsInSection;
+            }
+        }
+        return answer;
+    }
+
+public:
     // 9.Palindrome Number
     bool isPalindrome(int x) {
         if (x < 0) return false;
@@ -87,6 +140,46 @@ public:
         }
         return true;
     }
+
+public:
+    // 11.Container With Most Water
+    int maxArea(std::vector<int> &height) {
+        int left = 0, right = height.size() - 1, answer = 0, area;
+        while (left < right) {
+            area = std::min(height[left], height[right]) * (right - left);
+            answer = std::max(answer, area);
+            if (height[left] < height[right]) {
+                left += 1;
+            } else right -= 1;
+        }
+        return answer;
+    }
+
+public:
+    // 15.3Sum
+    std::vector<std::vector<int>> threeSum(std::vector<int> &nums) {
+        std::sort(nums.begin(), nums.end());
+        std::vector<std::vector<int>> answer;
+        for (int i = 0; i < nums.size(); i++) {
+            int sum = -nums[i];
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] == sum) {
+                    answer.emplace_back(std::vector<int>{nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (nums[left] + nums[right] < sum) left++;
+                else right--;
+            }
+            while (i + 1 < nums.size() && nums[i] == nums[i + 1]) i++;
+        }
+        return answer;
+    }
+
 };
+
 
 #endif //TESTLEETCODECXX_SOLUTIONS_H
--- src/tests.cpp
+++ src/tests.cpp
@@ -5,52 +5,75 @@
 
 TEST_CASE("1.Two Sum") {
     SUBCASE("Case 1") {
-        std::vector<int> nums = {2, 7, 11, 15};
+        std::vector<int> nums{2, 7, 11, 15};
         int target = 9;
-        std::vector<int> expected = {0, 1};
+        std::vector<int> expected{0, 1};
         CHECK(expected == Solution().twoSum(nums, target));
     }
     SUBCASE("Case 2") {
-        std::vector<int> nums = {3, 2, 4};
+        std::vector<int> nums{3, 2, 4};
         int target = 6;
-        std::vector<int> expected = {1, 2};
+        std::vector<int> expected{1, 2};
         CHECK(expected == Solution().twoSum(nums, target));
     }
     SUBCASE("Case 3") {
-        std::vector<int> nums = {3, 3};
+        std::vector<int> nums{3, 3};
         int target = 6;
-        std::vector<int> expected = {0, 1};
+        std::vector<int> expected{0, 1};
         CHECK(expected == Solution().twoSum(nums, target));
     }
 }
 
 TEST_CASE("2.Add Two Numbers") {
     SUBCASE("Case 1") {
-        std::vector<int> l1Array = {2, 4, 3};
-        std::vector<int> l2Array = {5, 6, 4};
-        std::vector<int> expectedArray = {7, 0, 8};
+        std::vector<int> l1Array{2, 4, 3}, l2Array{5, 6, 4}, expectedArray{7, 0, 8};
         ListNode *l1 = VectorToListNode(l1Array);
         ListNode *l2 = VectorToListNode(l2Array);
         ListNode *expected = VectorToListNode(expectedArray);
         CHECK(EqualListNode(Solution().addTwoNumbers(l1, l2), expected));
     }
     SUBCASE("Case 2") {
-        std::vector<int> l1Array = {0};
-        std::vector<int> l2Array = {0};
-        std::vector<int> expectedArray = {0};
+        std::vector<int> l1Array{0}, l2Array{0}, expectedArray{0};
         ListNode *l1 = VectorToListNode(l1Array);
         ListNode *l2 = VectorToListNode(l2Array);
         ListNode *expected = VectorToListNode(expectedArray);
         CHECK(EqualListNode(Solution().addTwoNumbers(l1, l2), expected));
     }
     SUBCASE("Case 3") {
-        std::vector<int> l1Array = {9, 9, 9, 9, 9, 9, 9};
-        std::vector<int> l2Array = {9, 9, 9, 9};
-        std::vector<int> expectedArray = {8, 9, 9, 9, 0, 0, 0, 1};
+        std::vector<int> l1Array{9, 9, 9, 9, 9, 9, 9}, l2Array{9, 9, 9, 9}, expectedArray{8, 9, 9, 9, 0, 0, 0, 1};
         ListNode *l1 = VectorToListNode(l1Array);
         ListNode *l2 = VectorToListNode(l2Array);
         ListNode *expected = VectorToListNode(expectedArray);
         CHECK(EqualListNode(Solution().addTwoNumbers(l1, l2), expected));
+    }
+}
+
+TEST_CASE("5.Longest Palindromic Substring") {
+    SUBCASE("Case 1") {
+        std::string s{"babad"}, expected{"aba"};
+        CHECK(Solution().longestPalindrome(s) == expected);
+    }
+    SUBCASE("Case 2") {
+        std::string s{"cbbd"}, expected{"bb"};
+        CHECK(Solution().longestPalindrome(s) == expected);
+    }
+}
+
+TEST_CASE("6.Zigzag Conversion") {
+    SUBCASE("Case 1") {
+        std::string s{"PAYPALISHIRING"}, expected{"PAHNAPLSIIGYIR"};
+        int numRows = 3;
+        CHECK(Solution().convert(s, numRows) == expected);
+    }
+    SUBCASE("Case 2") {
+        std::string s{"PAYPALISHIRING"}, expected{"PINALSIGYAHRPI"};
+        int numRows = 4;
+        CHECK(Solution().convert(s, numRows) == expected);
+    }
+    SUBCASE("Case 3") {
+        std::string s{"A"}, expected{"A"};
+        int numRows = 1;
+        CHECK(Solution().convert(s, numRows) == expected);
     }
 }
 
@@ -66,5 +89,37 @@ TEST_CASE("9.Palindrome Number") {
     SUBCASE("Case 3") {
         int x = 10;
         CHECK(!(Solution().isPalindrome(x)));
+    }
+}
+
+TEST_CASE("11.Container With Most Water") {
+    SUBCASE("Case 1") {
+        std::vector<int> height{1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int expected = 49;
+        CHECK(Solution().maxArea(height) == expected);
+    }
+    SUBCASE("Case 2") {
+        std::vector<int> height{1, 1};
+        int expected = 1;
+        CHECK(Solution().maxArea(height) == expected);
+    }
+}
+
+TEST_CASE("15.3Sum") {
+    SUBCASE("Case 1") {
+        std::vector<int> nums{-1, 0, 1, 2, -1, -4};
+        std::vector<std::vector<int>> expected{{-1, -1, 2},
+                                               {-1, 0,  1}};
+        CHECK(Solution().threeSum(nums) == expected);
+    }
+    SUBCASE("Case 2") {
+        std::vector<int> nums{0, 1, 1};
+        std::vector<std::vector<int>> expected{};
+        CHECK(Solution().threeSum(nums) == expected);
+    }
+    SUBCASE("Case 3") {
+        std::vector<int> nums{0, 0, 0};
+        std::vector<std::vector<int>> expected{{0, 0, 0}};
+        CHECK(Solution().threeSum(nums) == expected);
     }
 }
